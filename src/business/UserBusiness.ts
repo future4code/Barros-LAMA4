7027618c7d1f1aaa802d5062686731c26dafc482
Ricardo_Barros <ricardo_teleco@hotmail.com>
2023-03-12 22:58:02 -0300

    feat: foram criados os tratamentos de erro dos endpoints 1 e 2

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -3,41 +3,91 @@ import { UserDatabase } from "../data/UserDatabase";
 import { IdGenerator } from "../services/IdGenerator";
 import { HashManager } from "../services/HashManager";
 import { Authenticator } from "../services/Authenticator";
+import { CustomError, InvalidEmail, InvalidName, InvalidPassword, SmallEmail, UserNotFound } from "../error/BaseError";
 
 export class UserBusiness {
 
     async createUser(user: UserInputDTO) {
 
-        const idGenerator = new IdGenerator();
-        const id = idGenerator.generate();
+        try {
 
-        const hashManager = new HashManager();
-        const hashPassword = await hashManager.hash(user.password);
+            if(!user.email){
+                throw new InvalidEmail
+            }
 
-        const userDatabase = new UserDatabase();
-        await userDatabase.createUser(id, user.email, user.name, hashPassword, user.role);
+            const filterEmail = user.email.includes("@" && ".com")
+            if(filterEmail != true){
+                throw new InvalidEmail
+            }
 
-        const authenticator = new Authenticator();
-        const accessToken = authenticator.generateToken({ id, role: user.role });
+            if(!user.name){
+                throw new InvalidName
+            }
 
-        return accessToken;
+            if(user.email.length < 10){
+                throw new SmallEmail
+            }
+
+            if(user.name.length < 4){
+                throw new InvalidName
+            }
+
+            if(!user.password){
+                throw new InvalidPassword
+            }
+
+            if(user.password.length < 6){
+                throw new InvalidPassword
+            }   
+
+            const idGenerator = new IdGenerator();
+            const id = idGenerator.generate();
+
+            const hashManager = new HashManager();
+            const hashPassword = await hashManager.hash(user.password);
+
+            const userDatabase = new UserDatabase();
+            await userDatabase.createUser(id, user.email, user.name, hashPassword, user.role);
+
+            const authenticator = new Authenticator();
+            const accessToken = authenticator.generateToken({ id, role: user.role });
+
+            return accessToken;
+
+        } catch (error:any) {
+            throw new CustomError(error.status, error.message)
+        }
     }
 
     async getUserByEmail(user: LoginInputDTO) {
 
-        const userDatabase = new UserDatabase();
-        const userFromDB = await userDatabase.getUserByEmail(user.email);
+        try {
 
-        const hashManager = new HashManager();
-        const hashCompare = await hashManager.compare(user.password, userFromDB.getPassword());
+            if(!user.email){
+                throw new UserNotFound
+            }
 
-        const authenticator = new Authenticator();
-        const accessToken = authenticator.generateToken({ id: userFromDB.getId(), role: userFromDB.getRole() });
+            if(!user.password){
+                throw new InvalidPassword
+            }
 
-        if (!hashCompare) {
-            throw new Error("Invalid Password!");
+            const userDatabase = new UserDatabase();
+            const userFromDB = await userDatabase.getUserByEmail(user.email);
+
+            const hashManager = new HashManager();
+            const hashCompare = await hashManager.compare(user.password, userFromDB.getPassword());
+
+            const authenticator = new Authenticator();
+            const accessToken = authenticator.generateToken({ id: userFromDB.getId(), role: userFromDB.getRole() });
+
+            if (!hashCompare) {
+                throw new Error("Invalid Password!");
+            }
+
+            return accessToken;
+
+        } catch (error:any) {
+            throw new CustomError(error.status, error.message)
         }
-
-        return accessToken;
     }
 }
