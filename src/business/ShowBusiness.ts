6fd8418e0205b5c84fbd61ca69e6626fb5529da5
Mauricio Luiz de toledo <102395706+mlt3800@users.noreply.github.com>
2023-03-15 13:51:52 +0000

    Refatorando bugs

--- src/business/ShowBusiness.ts
+++ src/business/ShowBusiness.ts
@@ -1,42 +1,57 @@
 import { ShowDataBase } from "../data/ShowDataBase";
-import { ICreateShowInputDTO, ICreateShowOutputDTO, Show } from "../model/Show";
+import { ICreateShowInputDTO, ICreateShowOutputDTO, ICreateShowtDTO, ShowDTO } from "../model/Show";
 import { Authenticator } from "../services/Authenticator";
 import { IdGenerator } from "../services/IdGenerator";
+import { IdAuthenticator } from "../model/IdAuthenticator";
 import { CustomError } from "../error/BaseError";
+import { MissingToken } from "../error/BaseError";
+import { IdNotFound, BandId } from "../error/BandErrors";
+import { GeneratorId } from "../model/GeneratorId";
+import { ShowsRepository } from "../model/ShowRepository";
+import { BandRepository } from "../model/BandRepository";
+import { UserRole } from "../model/User";
+import { WeekDay, StartTime, EndTime, InvalidWeekDay } from "../error/ShowError";
+import { Unauthorized } from "../error/BaseError";
+
 
 export class ShowBusiness {
+    constructor(
+        private ShowDataBase : ShowsRepository,
+        private bandDataBase : BandRepository,
+        private generateId : GeneratorId,
+        private auth : IdAuthenticator,
+        private ShowTime : ICreateShowtDTO
+    ) {}
 
-    public createShow = async (input: ICreateShowInputDTO): Promise<ICreateShowOutputDTO> => {
-        const showDataBase = new ShowDataBase
-        const authenticator = new Authenticator
-        const idGenerator = new IdGenerator
-
-
+    async createShow (input: ICreateShowtDTO): Promise<void> {
         try {
-            const { token, band, startsAt } = input
+            if (!input.token) {
+                throw new MissingToken()
+            }
+            if (!input.bandId) {
+                throw new BandId()
+            }
+            if (!input.weekDay) {
+                throw new WeekDay()
+            }
+            if (!input.startTime) {
+                throw new StartTime()
+            }
+            if (!input.endTime) {
+                throw new EndTime()
+            }
+            if (input.weekDay.toLowerCase() !== "sexta" && input.weekDay.toLowerCase() !== "sábado" && input.weekDay.toLowerCase() !== "domingo") {
+                throw new InvalidWeekDay()
+            }
 
-        const payload = authenticator.getTokenData(token)
+            const {id, role} = await this.auth.getTokenData(input.token)
+            if (role.toUpperCase() !== UserRole.ADMIN) {
+                throw new Unauthorized()
+            }
 
-        const startsAtDate = new Date(startsAt)
-        
-
-        const show = new Show(
-            idGenerator.generateId(),
-            band,
-            startsAtDate
-        )
-
-        await showDataBase.createShow(show)
-
-        const response: ICreateShowOutputDTO = {
-            message: "Show criado com sucesso",
-            show
+            const bandIdExists = await this.bandDataBase.getBandBy("id", input.bandId)
+            if (!bandIdExists) {
+                throw new IdNotFound()
+            }
+           
         }
-
-        return response
-
-        } catch (error: any) {
-            throw new CustomError(error.statusCode, error.message)
-        }
-    }
-}
